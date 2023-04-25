3dd92916474b1455b45f5efa261b34f236e2ff50
Diego Gutierrez <diego.guiterrez@lucidworks.com>
2023-04-25 11:08:20 -0500

    initial commit

---
+++ src/main/java/com/example/testprometheus/TestPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPrometheusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testprometheus/TestPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
